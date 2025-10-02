baba5062c7de2776a73801ef83ac739db62d92c8
ThomasAguirre <tomasesteban20010@gmail.com>
2025-10-01 19:36:49 -0500

    se añaden nuevas librerias necesarias

--- app/src/main/java/com/example/nuevoproyectoprueba/ui/screens/MenuScreen.kt
+++ app/src/main/java/com/example/nuevoproyectoprueba/ui/screens/MenuScreen.kt
@@ -1,6 +1,7 @@
 package com.example.nuevoproyectoprueba.ui.screens
 
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
