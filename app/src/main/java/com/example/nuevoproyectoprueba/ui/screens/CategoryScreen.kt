bf5848b88db03223d8500278808b70320123e4f1
ThomasAguirre <tomasesteban20010@gmail.com>
2025-10-01 19:30:38 -0500

    importaciones correcion y librerias necesarias

--- app/src/main/java/com/example/nuevoproyectoprueba/ui/screens/CategoryScreen.kt
+++ app/src/main/java/com/example/nuevoproyectoprueba/ui/screens/CategoryScreen.kt
@@ -1,7 +1,13 @@
 package com.example.nuevoproyectoprueba.ui.screens
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
