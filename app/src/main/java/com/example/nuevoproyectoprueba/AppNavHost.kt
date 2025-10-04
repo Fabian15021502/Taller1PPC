4c98b29708f9b6082a86957812037fd8ed6946a2
Fabian15021502 <fabianalexandervanegasbeltran@gmail.com>
2025-10-03 20:03:04 -0500

    Importamos navArgument

--- app/src/main/java/com/example/nuevoproyectoprueba/AppNavHost.kt
+++ app/src/main/java/com/example/nuevoproyectoprueba/AppNavHost.kt
@@ -4,7 +4,7 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import com.example.nuevoproyectoprueba.ui.screens.MenuScreen
+import androidx.navigation.navArgument
 import com.example.nuevoproyectoprueba.ui.screens.CategoryScreen
 import com.example.nuevoproyectoprueba.ui.screens.GameScreen
 import com.example.nuevoproyectoprueba.ui.screens.MenuScreen
