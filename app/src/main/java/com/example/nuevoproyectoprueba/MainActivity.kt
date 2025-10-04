9452f6753bea9e55cf7095236c1019c8a6aa14bb
Fabian15021502 <fabianalexandervanegasbeltran@gmail.com>
2025-10-03 19:51:21 -0500

    Usamos AppNavHost para iniciar

--- app/src/main/java/com/example/nuevoproyectoprueba/MainActivity.kt
+++ app/src/main/java/com/example/nuevoproyectoprueba/MainActivity.kt
@@ -23,7 +23,7 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     val navController = rememberNavController()
-                    AppNavHost(navController)
+                    AppNavHost(navController = navController, startDestination = "menu")
                 }
             }
         }
