9df71089bf8c9d894757942ef54ba09e20ab0217
Fabian15021502 <fabianalexandervanegasbeltran@gmail.com>
2025-10-03 20:03:16 -0500

    Importamos NavType

--- app/src/main/java/com/example/nuevoproyectoprueba/AppNavHost.kt
+++ app/src/main/java/com/example/nuevoproyectoprueba/AppNavHost.kt
@@ -2,6 +2,7 @@ package com.example.nuevoproyectoprueba
 
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
